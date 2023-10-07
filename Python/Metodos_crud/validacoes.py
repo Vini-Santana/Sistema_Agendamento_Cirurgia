974507f0edcd26521d6ae7dbfa3c8c3ce1c2ff28
Vinicius Vanderlei Santana <vinisantana1112@gmail.com>
2023-10-07 16:35:10 -0300

    Insernido métodos que valida se tem profissionais disponíveis para a cirurgia

--- Python/Metodos_crud/validacoes.py
+++ Python/Metodos_crud/validacoes.py
@@ -10,23 +10,7 @@ def validaUsuarioSenha_RetornaNivelAcesso(usuario, senha):
         raise ValueError("Usuário ou senha incorretos")
     else:
         return True
-
-#VALIDAÇÃO DO PARÂMETRO EM OUTRAS CIRURGIAS
-#CIRURGIÃO
-def validaCirurgiaoEmOutraCirurgia(dtInicio, dtFim):
-    consulta = f'SELECT FKCIRURGIAO FROM cirurgia WHERE ("{dtInicio}" > DTINICIO AND "{dtInicio}" < DTFIM) OR ("{dtFim}" > DTINICIO AND "{dtFim}" < DTFIM) OR ("{dtInicio}" < DTINICIO AND "{dtFim}" > DTFIM)'
-    conexao.cursor.execute(consulta)
-    resultadoFim = conexao.cursor.fetchall()
-    if resultadoFim == []:
-        return resultadoFim
-    else:
-        raise ReferenceError(f"O Cirurgião não está disponível na data marcada({dtInicio} - {dtFim})")
-    #VALIDAR SE A FKRETORNADA ESTÁ EM OUTRA CIRURGIA
-    
-    
-    
-#print(validaParametroEntreDtInicioDtfimCom("2023/02/03 04:03:03", "2023/02/03 07:03:03"))
-
+#VALIDA SE A DATA DE INÍCIO DA CIRURGIA É MENOR QUE DATA FIM
 def validaDtinicioMenorDtfim(DtInicio, DtFim):
     
     if datetime.strptime(f"{DtInicio}",  "%Y/%m/%d %H:%M:%S") > datetime.strptime(f"{DtFim}",  "%Y/%m/%d %H:%M:%S"):
@@ -34,7 +18,44 @@ def validaDtinicioMenorDtfim(DtInicio, DtFim):
     else:
         return True
     
-print(validaCirurgiaoEmOutraCirurgia("2023/02/03 06:03:03", "2023/02/03 12:03:03"))
+    
+#VALIDA SE TEM SALA, CIRURGIÃO, ANESTESISTA, INSTRUMENTADOR E ENFERIMEIRO DISPONÍVEIS PARA CIRURGIA
+def validaProfissionaisESalaDisponiveis(dtInicio, dtFim):
+    consulta = f'SELECT IDSALA FROM SALA WHERE IDSALA NOT IN (SELECT FKSALA FROM CIRURGIA WHERE ("{dtInicio}" > DTINICIO AND "{dtInicio}" < DTFIM) OR ("{dtFim}" > DTINICIO AND "{dtFim}" < DTFIM) OR ("{dtInicio}" < DTINICIO AND "{dtFim}" > DTFIM))'
+    conexao.cursor.execute(consulta)
+    resultadoFim = conexao.cursor.fetchall()
+    msg =[]
+    if resultadoFim == []:
+        msg.append("Nenhuma sala de cirurgia disponível na data inserida")
+        
+    consulta = f'SELECT IDCIRURGIAO FROM CIRURGIAO WHERE IDCIRURGIAO NOT IN (SELECT FKCIRURGIAO FROM CIRURGIA WHERE ("{dtInicio}" > DTINICIO AND "{dtInicio}" < DTFIM) OR ("{dtFim}" > DTINICIO AND "{dtFim}" < DTFIM) OR ("{dtInicio}" < DTINICIO AND "{dtFim}" > DTFIM))'
+    conexao.cursor.execute(consulta)
+    resultadoFim = conexao.cursor.fetchall()
+    if resultadoFim == []:
+        msg.append("Nenhum cirurgião disponível na data inserida")
+        
+    consulta = f'SELECT IDANESTESISTA FROM ANESTESISTA WHERE IDANESTESISTA NOT IN (SELECT FKANESTESISTA FROM CIRURGIA WHERE ("{dtInicio}" > DTINICIO AND "{dtInicio}" < DTFIM) OR ("{dtFim}" > DTINICIO AND "{dtFim}" < DTFIM) OR ("{dtInicio}" < DTINICIO AND "{dtFim}" > DTFIM))'
+    conexao.cursor.execute(consulta)
+    resultadoFim = conexao.cursor.fetchall()
+    if resultadoFim == []:
+        msg.append("Nenhum anestesista disponível na data inserida")
+        
+    consulta = f'SELECT IDINSTRUMENTADOR FROM INSTRUMENTADOR WHERE IDINSTRUMENTADOR NOT IN (SELECT FKINSTRUMENTADOR FROM CIRURGIA WHERE ("{dtInicio}" > DTINICIO AND "{dtInicio}" < DTFIM) OR ("{dtFim}" > DTINICIO AND "{dtFim}" < DTFIM) OR ("{dtInicio}" < DTINICIO AND "{dtFim}" > DTFIM))'
+    conexao.cursor.execute(consulta)
+    resultadoFim = conexao.cursor.fetchall()
+    if resultadoFim == []:
+        msg.append("Nenhum instrumentador disponível na data inserida")
+        
+    consulta = f'SELECT IDENFERMEIRO FROM ENFERMEIRO WHERE IDENFERMEIRO NOT IN (SELECT AO.FKENFERMEIRO FROM CIRURGIA_ENFERMEIRO AO, CIRURGIA A WHERE ("{dtInicio}" > A.DTINICIO AND "{dtInicio}" < A.DTFIM) OR ("{dtFim}" > A.DTINICIO AND "{dtFim}" < A.DTFIM) OR ("{dtInicio}" < A.DTINICIO AND "{dtFim}" > A.DTFIM))'
+    conexao.cursor.execute(consulta)
+    resultadoFim = conexao.cursor.fetchall() 
+    if resultadoFim == []:
+        msg.append("Nenhum enfermeiro disponível na data inserida")
+        
+    if msg != []:
+        raise ValueError(msg)
+    else:
+        return True
 
 
 
